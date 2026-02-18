9f99f9b8ed0068bdd25c461c9963070473b75e51
Jessé Silva <jessecode26@gmail.com>
2026-02-17 21:21:16 -0300

    feat: pratica de loops for e while

--- condicionais/script.js
+++ condicionais/script.js
@@ -3,28 +3,63 @@
 // - Uso de else if
 // - Operadores relacionais (>, <, >=, <=, ===, !==)
 // Condição simples
-let idade = 18;
+// switch
+// let idade = 18;
 
-if (idade >= 18) {
-  console.log("Maior de idade");
+// if (idade >= 18) {
+//   console.log("Maior de idade");
+// }
+
+// // If e Else
+// let nota = 6;
+
+// if (nota >= 5) {
+//   console.log("Aprovado");
+// } else {
+//   console.log("Reprovado");
+// }
+
+// // Else if
+// let hora = 24;
+
+// if (hora < 19) {
+//   console.log("Bom dia");
+// } else if (hora < 18) {
+//   console.log("Boa tarde");
+// } else {
+//   console.log("Boa noite");
+// }
+
+// TESTANDO LOGICAS
+
+let nota1 = 5;
+let nota2 = 8;
+
+let conceito;
+
+let media = (nota1 + nota2) / 2;
+
+if (media >= 8) {
+  conceito = "otimo";
+} else if (media >= 6.5) {
+  conceito = "bom";
+} else {
+  conceito = "Regular";
 }
 
-// If e Else
-let nota = 6;
+console.log(media);
+console.log(conceito);
 
-if (nota >= 5) {
-  console.log("Aprovado");
-} else {
-  console.log("Reprovado");
-}
-
-// Else if
-let hora = 24;
-
-if (hora < 19) {
-  console.log("Bom dia");
-} else if (hora < 18) {
-  console.log("Boa tarde");
-} else {
-  console.log("Boa noite");
+switch (conceito) {
+  case "otimo":
+    console.log("Parabens vc é um otmo aluno");
+    break;
+  case "bom":
+    console.log("vc precisa estudar mais");
+    break;
+  case "Regular":
+    console.log("voce nao atingiu nota suficiente está REPROVADO");
+    break;
+  default:
+    console.log("DEU ERRO");
 }
---
+++ repeticao/script.js
@@ -0,0 +1,24 @@
+// Loop for e while
+
+// let numero = 5;
+
+//for (let i = 1; i < numero; i++) {
+//console.log("Executando Código " + i + " res");
+//}
+
+// while (numero < 100) {
+//   console.log("Número " + numero);
+//   numero++;
+// }
+
+// console.log("acabou");
+
+let numero2 = Math.random() * 100;
+
+while (numero2 < 90) {
+  console.log(numero2);
+  numero2 = Math.random() * 100;
+}
+
+console.log(numero2);
+console.log("acabou");
